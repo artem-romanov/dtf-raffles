e6ec7482bdcb38db812784c25f0b7d6359e0a70e
Artem Romanov <xenox2048@gmail.com>
2026-01-11 19:07:52 +0300

    Micro-optimize IsTooLongForTelegramPost func

--- internal/telegram/utils/utils.go
+++ internal/telegram/utils/utils.go
@@ -1,13 +1,12 @@
 package telegram_utils
 
+import "unicode/utf8"
+
 const (
 	MaxPostCharacters = 4096
 )
 
 func IsTooLongForTelegramPost(s string) bool {
-	strLength := len([]rune(s))
-	if strLength > MaxPostCharacters {
-		return true
-	}
-	return false
+	strLength := utf8.RuneCountInString(s)
+	return strLength > MaxPostCharacters
 }
